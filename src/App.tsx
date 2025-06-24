f906015beb807941a676eb8c412e79349e8e6c57
getonow <brenda.delafuente@getonow.com>
2025-06-24 03:41:22 +0200

    Set up for GitHub Pages deployment at /akaipilot1-frontend/

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -16,7 +15,8 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        {/* No basename needed for custom domain */}
+        <BrowserRouter basename="/akaipilot1-frontend/">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
